818d3ca7503f58849906484b99645ff070816b37
MGarc758 <83996652+MGarc758@users.noreply.github.com>
2023-04-26 21:44:24 -0700

    More files added
    Added files for text so we could add tooltips

--- Assets/Scripts/ItemDisplay.cs
+++ Assets/Scripts/ItemDisplay.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,8 +9,12 @@ public class ItemDisplay : MonoBehaviour
 {
     public ItemInfo itemToDisplay;
 
-    private Image icon;
+    public TextMeshProUGUI hoverText;
+
+    public float textOffset = 0f;
     
+    private Image icon;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +34,24 @@ public class ItemDisplay : MonoBehaviour
         {
             icon.enabled = false;
         }
+    }
+
+    private void EnableToolTip()
+    {
+        Debug.Log("Mouse is over object");
+        if (itemToDisplay != null)
+        {
+            var mousePos = Input.mousePosition;
+
+            hoverText.text = itemToDisplay.description;
+            hoverText.transform.position = new Vector3(mousePos.x + textOffset,
+                mousePos.y, mousePos.z);
+            hoverText.enabled = true;
+        }
+    }
+
+    private void DisableToolTip()
+    {
+        hoverText.enabled = false;
     }
 }
