ca4405bcf3f4ab7a18274e9619aa770dc593453a
Marcos Arroyo <Maarroyo@csumb.edu>
2023-05-03 22:32:34 -0700

    Shooter enemy
    This contains the Marksmen & bullet prefabs. it also uses three scripts one is the detection for the marksmen, movement for the enemy(uses the default one from "Enemies Design" discord chat), a script for the bullets to destroy to console log to player when shot.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    
+    public float speed = 1.0f;
+    public bool edge;
+    public bool wall;
+    private float walkDir = 1.0f;
+    private Rigidbody enemyRB;
+    private GameObject enemy;
+    private Vector3 enemyPos;
+    public bool isGrounded;
+
+    private void Start()
+    {
+        enemy = this.gameObject;
+        enemyRB = this.gameObject.GetComponent<Rigidbody>();
+        enemyPos = enemy.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        enemyPos = enemy.transform.position;
+        if ((wall || !edge) && isGrounded)
+        {
+            if (walkDir > 0)
+            {
+                this.gameObject.transform.localRotation = Quaternion.Euler(0,0,0);
+            }
+            if (walkDir < 0)
+            {
+                this.gameObject.transform.localRotation = Quaternion.Euler(0,180,0);
+            }
+            walkDir = -walkDir;
+        }
+    }
+
+    private void Update()
+    {
+        edge = Physics.Raycast(enemyPos - new Vector3(0, 0.5f, 0), new Vector3(1 * walkDir, -0.75f, 0), 1f);
+        wall = Physics.Raycast(enemyPos - new Vector3(0, 0.8f, 0), new Vector3(1 * walkDir, 0, 0), 0.8f);
+        isGrounded = Physics.Raycast(enemyPos, Vector3.down, 1.2f);
+        if (!isGrounded)
+        {
+            enemyRB.velocity = new Vector3(0, -10f, 0);
+        }
+        else
+        {
+            enemyRB.velocity = new Vector3(speed * walkDir, 0, 0);
+        }
+        
+    }
+}
