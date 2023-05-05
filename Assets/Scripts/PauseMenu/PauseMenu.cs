dab8d28fc260a4452c9a75e26ae1bafa9467f1f6
Marcos Arroyo <Maarroyo@csumb.edu>
2023-05-05 12:42:32 -0700

    Added Pause menu + missing sounds.meta for main menu
    This has The Script for the pausemenu, the prefab for the menu to apply anywhere, and sounds from main menu.

---
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,91 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject pauseMenuUI;
+    public AudioMixer mainMixer;
+    public Button hubButton;
+    public Button soundsButton;
+    public Button exitButton;
+    public GameObject audioSettingsPanel;
+    
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        hubButton.onClick.AddListener(TeleportToScene);
+        exitButton.onClick.AddListener(ExitGame);
+        audioSettingsPanel.SetActive(false);
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.pointerPress == soundsButton.gameObject)
+        {
+            ToggleAudioSettings();
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void TeleportToScene()
+    {
+        Resume();
+        SceneManager.LoadScene("Hub");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void ToggleAudioSettings()
+    {
+        Debug.Log("ToggleAudioSettings called");
+        if (audioSettingsPanel.activeSelf == true)
+        {
+            audioSettingsPanel.SetActive(false);
+            Debug.Log("Panel deactivated");
+        }
+        else
+        {
+            audioSettingsPanel.SetActive(true);
+            Debug.Log("Panel activated");
+        }
+    }
+
+
+    public void SetOverallVolume(float volume)
+    {
+        mainMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+    
+}
