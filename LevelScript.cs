60435496dd81c9699c0ac967c6a60c150423d23d
immuneshrimp3 <72824377+immuneshrimp3@users.noreply.github.com>
2023-05-10 05:03:27 -0700

    Level Script Layout
    Created the level script layout with working XP for the movement levels

---
+++ LevelScript.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScript : MonoBehaviour
+{
+    public int movementLVL;
+    public int combatLVL;
+    public int xpToNext1; //Movement
+    public int xpToNext2; //Combat
+    public int currXP1;
+    public int currXP2;
+    public int skillPoints1; //Movement
+    public int skillPoints2; //Combat
+    private PlayerMovement PM;
+    private GameObject player;
+    private float currSpeed;
+    private float defTick = 5f;
+    private float tick1 = 5f;
+    private float tick2 = 5f;
+    private float tick3 = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.gameObject;
+        PM = player.GetComponent<PlayerMovement>();
+        
+        //Data to be saved
+        movementLVL = 1;
+        combatLVL = 1;
+        xpToNext1 = 30 * movementLVL;
+        xpToNext2 = 15 * combatLVL;
+        currXP1 = 0; //For movement
+        currXP2 = 0; //For combat
+        skillPoints1 = 0;
+        skillPoints2 = 0;
+        ////////////////////////////
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement Level Code
+        currSpeed = PM.pVelocity;
+        if (currSpeed >= 3 && currSpeed < 6)
+        {
+            tick1 -= Time.deltaTime;
+            if (tick1 <= 0)
+            {
+                currXP1 += 1;
+                tick1 = defTick;
+            }
+        }
+        else if (currSpeed >= 6 && currSpeed < 12)
+        {
+            tick2 -= Time.deltaTime;
+            if (tick2 <= 0)
+            {
+                currXP1 += 3;
+                tick2 = defTick;
+            }
+        }
+        else if (currSpeed >= 12)
+        {
+            tick3 -= Time.deltaTime;
+            if (tick3 <= 0)
+            {
+                currXP1 += 5;
+                tick3 = defTick;
+            }
+        }
+        ////////////////////////////
+        //Check xp for Level Up
+        if (currXP1 >= xpToNext1)
+        {
+            currXP1 = currXP1 % xpToNext1;
+            movementLVL += 1;
+            xpToNext1 = 30 * movementLVL;
+            skillPoints1 += 1;
+        }
+        Debug.Log("current XP: " + currXP1 + "  |  current LVL: " + movementLVL);
+    }
+
+    public void addXP() //This function is for combat xp only;
+    {
+        
+    }
+}
